b57b401ae78343340a8f060b19a5254ece1734de
EphimSh <ephimizm@gmail.com>
2025-10-13 14:04:34 +0300

    add task1 solution

---
+++ src/tests/herokuapp_test.spec.ts
@@ -0,0 +1,28 @@
+import test, { expect } from "@playwright/test";
+import { URL } from "../test-data/consts/url/Url";
+
+test.describe("[HEROKU] [DYNAMIC CONTROLS] [WAITS]", () => {
+  test("", async ({ page }) => {
+    await test.step("Открыть the-internet.herokuapp.com", async () => {
+      await page.goto(`${URL.HEROKU_APP_BASE_URL}`);
+    });
+    const dynamicControlsLink = page.locator('a[href="/dynamic_controls"]');
+    const removeButton = page.getByRole("button", { name: "Remove" });
+    const checkBox = page.locator('input[type="checkbox"]');
+    const pageMainHeader = page.locator(".example h4").first();
+    const addButton = page.getByRole("button", { name: "Add" });
+    const message = page.locator("#message");
+
+    dynamicControlsLink.click();
+    await expect(removeButton).toBeVisible();
+    await expect(pageMainHeader).toHaveText("Dynamic Controls");
+    checkBox.click();
+    removeButton.click();
+    await checkBox.waitFor({ state: "hidden" });
+    await addButton.waitFor({ state: "visible" });
+    await expect(message).toHaveText("It's gone!");
+    await addButton.click();
+    await expect(checkBox).toBeVisible({ timeout: 5100 });
+    await expect(message).toHaveText("It's back!");
+  });
+});
