bc656336129d6433d0c1fb1b3ce82a1980f7ce02
EphimSh <ephimizm@gmail.com>
2025-10-28 21:10:13 +0300

    add: tableRow parse function, check table-row data test

--- src/tests/herokuapp_test.spec.ts
+++ src/tests/herokuapp_test.spec.ts
@@ -1,5 +1,9 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, Page } from "@playwright/test";
 import { URL } from "../test-data/consts/url/Url";
+import {
+  IHerokuTablesUser,
+  usersData,
+} from "test-data/consts/heroku-data-tables/HerokuTablesUser";
 
 test.describe("[HEROKU] [DYNAMIC CONTROLS] [WAITS]", () => {
   test("", async ({ page }) => {
@@ -25,4 +29,38 @@ test.describe("[HEROKU] [DYNAMIC CONTROLS] [WAITS]", () => {
     await expect(checkBox).toBeVisible({ timeout: 5100 });
     await expect(message).toHaveText("It's back!");
   });
+});
+
+test.describe("[HEROKU] [TABLES]", async () => {
+  usersData.forEach((user) => {
+    test(`Check table-row data with user email: ${user.email}`, async ({
+      page,
+    }) => {
+      await page.goto(`${URL.HEROKU_APP_BASE_URL}tables`, {
+        waitUntil: "domcontentloaded",
+      });
+
+      const tableRow = await getTableRow(page, user.email);
+      expect(tableRow).toEqual(user);
+    });
+  });
+
+  async function getTableRow(
+    page: Page,
+    email: string
+  ): Promise<IHerokuTablesUser> {
+    const [lastname, firstname, _email, due, website] = await page
+      .locator("#table2 tbody tr", {
+        has: page.locator("td", { hasText: email }),
+      })
+      .locator("td")
+      .allInnerTexts();
+    return {
+      lastName: lastname!,
+      firstName: firstname!,
+      email: _email!,
+      due: due!,
+      website: website!,
+    };
+  }
 });
