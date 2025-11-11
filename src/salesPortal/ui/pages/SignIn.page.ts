9561bcca8d936432624dc676340b308572d5664f
EphimSh <ephimizm@gmail.com>
2025-11-11 22:12:05 +0300

    add ui: pages, add-product modal window

---
+++ src/salesPortal/ui/pages/SignIn.page.ts
@@ -0,0 +1,30 @@
+import { Locator } from "playwright/test";
+import { SalesPortalPage } from "./SalesPortalPage.page";
+import { ICredentials } from "test-data/consts/salesPortal/data/credentials/Credential.type";
+
+export class SignInPage extends SalesPortalPage {
+  uniqueElement: Locator = this.page.locator("form", {
+    has: this.page.locator("p .lead", { hasText: "Sign in with" }),
+  });
+  readonly loginForm = this.page.locator("form");
+  readonly emailInput = this.loginForm.locator("input#emailinput");
+  readonly passwordInput = this.loginForm.locator("input#passwordinput");
+  readonly rememberMeCheckbox = this.loginForm.locator(
+    "input#remembermecheckbox"
+  );
+  readonly loginButton = this.loginForm.locator("button", { hasText: "Login" });
+
+  async fillForm(credentials: Partial<ICredentials>) {
+    if (credentials.username) await this.emailInput.fill(credentials.username);
+    if (credentials.password)
+      await this.passwordInput.fill(credentials.password);
+  }
+
+  async clickOnRememberMe() {
+    await this.rememberMeCheckbox.check();
+  }
+
+  async clickOnLoginButton() {
+    await this.loginButton.click();
+  }
+}
