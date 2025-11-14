2623f4558470772d1776aebf6203c28fe435a55e
EphimSh <ephimizm@gmail.com>
2025-11-15 00:45:19 +0300

    move files to consts dir & add types, schemas: Product, User, Login

---
+++ src/consts/salesPortal/data/types/product/Product.type.ts
@@ -0,0 +1,29 @@
+import { ID, IResponseFields } from "../core.type";
+import { MANUFACTURERS } from "../../product/Manufacturers";
+
+export interface IProduct {
+  name: string;
+  manufacturer: MANUFACTURERS;
+  price: number;
+  amount: number;
+  notes?: string;
+}
+
+export interface ICreatedOn {
+  createdOn: string;
+}
+
+export interface IProductInTable
+  extends Pick<IProduct, "name" | "price" | "manufacturer">,
+    ICreatedOn {}
+
+export interface IProductDetails extends Required<IProduct>, ICreatedOn {}
+
+export interface IProductFromResponse
+  extends Required<IProduct>,
+    ICreatedOn,
+    ID {}
+
+export interface IProductResponse extends IResponseFields {
+  Product: IProductFromResponse;
+}
---
+++ src/consts/url/Url.ts
@@ -0,0 +1,4 @@
+export enum URL {
+  BASE_URL = "https://anatoly-karpovich.github.io",
+  HEROKU_APP_BASE_URL = "https://the-internet.herokuapp.com/"
+}
